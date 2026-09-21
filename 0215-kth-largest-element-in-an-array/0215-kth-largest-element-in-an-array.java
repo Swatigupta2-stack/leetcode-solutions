2bfff7effcd86e4732ddcde7e433d03e75144196
Swati <sg7254@srmist.edu.in>
2026-09-21 23:51:03 +0530

    Time: 72 ms (25.93%), Space: 73.9 MB (86.69%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -10,3 +10,41 @@ class Solution {
       return minHeap.peek(); 
     }
 }
+/*
+a min-heap by default.
+
+That means:
+
+The smallest element always stays at the top.
+
+Example:
+
+PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+pq.add(50);
+pq.add(10);
+pq.add(30);
+pq.add(20);
+
+Internally, you don't need to think of it as fully sorted.
+
+But:
+
+pq.peek()
+
+will always give:
+
+10
+
+because 10 is the smallest.
+
+And:
+
+pq.poll()
+
+will remove and return the smallest element:
+
+poll() → 10
+
+Now the smallest remaining element becomes the top.
+*/
