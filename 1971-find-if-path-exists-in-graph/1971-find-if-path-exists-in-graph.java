eccd04d004cee773828b785b85da6fab5df35c7b
Swati <sg7254@srmist.edu.in>
2026-09-24 00:02:19 +0530

    Time: 53 ms (89.45%), Space: 281.6 MB (59.75%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
      
-     
+     //Create graph
      List<Integer>[] graph = new ArrayList[n];
-
+     //initialize every list
      for(int i=0; i<n; i++){
         graph[i] = new ArrayList<>();
      }
-
+      //add edges to graph
      for(int[] edge: edges){
         int u = edge[0];
         int v = edge[1];
 
         graph[u].add(v);
         graph[v].add(u);
+        //Why both?
+     //Because this is an undirected graph.
      }
 
      Queue<Integer> q = new ArrayDeque<>();
@@ -36,3 +38,30 @@ class Solution {
       return false;
     }
 }
+/*
+edges = [[0,1], [0,2], [1,2]]
+
+This means:
+
+0 ─── 1
+ \   /
+   2
+
+   Each edges[i] contains two connected nodes:
+
+edges[i][0]
+edges[i][1]
+
+So we create:
+
+List<Integer>[] graph = new ArrayList[n];
+
+Then initialize each list
+Then graph[node] works
+
+Suppose:
+
+graph[0] = [1, 2]
+graph[1] = [0, 2]
+graph[2] = [0, 1]
+*/
